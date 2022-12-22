8d06f4072b13395311903056b68fafa2625a2647
whitefox <whitefox061@gmail.com>
2022-12-23 00:25:17 +0700

    mpi: add 8

--- results.sh
+++ results.sh
@@ -6,6 +6,7 @@ do
     ./quicksort.out $n >> ./results.txt
     mpiexec -n 2 ./quicksort-mpi.out $n >> ./results.txt
     mpiexec -n 4 ./quicksort-mpi.out $n >> ./results.txt
+    mpiexec -n 8 ./quicksort-mpi.out $n >> ./results.txt
 done
 
 rm ./quicksort.out
